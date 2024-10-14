a270f64be02a41d2518ae172f3b952ae11ce3a54
Iris <iris152568@gmail.com>
2024-10-14 23:55:03 +0800

    基本数据类型1

---
+++ Code/src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        int a = 2147483647;
+        //a = a + 1;
+        int x = a + 1;
+        int b = a + 2;
+        int c = a + 3;
+        System.out.println(a);
+        System.out.println(x);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
